5d0296212a817d3a5b58334106d4521a2b4a8c45
B216 Instructor <30278906+evan-mitchell93@users.noreply.github.com>
2023-11-22 14:04:39 -0500

    docker in-class example

--- m05_docker/app.py
+++ m05_docker/app.py
@@ -1,10 +1,7 @@
-#numpy is a library used for working with arrays, linear algebra, and others.
-#arrays may be faster than lists because they are stored in contiguous memory locations (easier to find the next element)
-#It was also optimized for newer CPU architectures.
-
 import numpy as np
 
 arr = np.array([[0,0,0],[0,0,0],[0,0,0]])
+
 print(arr)
 
 for x in range(arr.shape[0]):
